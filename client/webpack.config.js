cfd1761ec22bfba2ca7c3edc86302a682866deeb
Arthur Henningfield <144608814+kylatae@users.noreply.github.com>
2023-12-02 00:04:02 -0600

    Fixed typo in webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,20 +22,20 @@ module.exports = () => {
       }),
 
       new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js"
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
       }),
 
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Just Another Text Editor",
-        short_name: "J.A.T.E",
-        description: "A simple text editor usable both online and offline",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
-        start_url: "/",
-        public_path: "/",
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'A simple text editor usable both online and offline',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
         icons: [{
           src: path.resolve('src/images/logo.png'),
           sizes: [96, 128, 192, 256, 384, 512],
